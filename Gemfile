e4e6e4091a7f4b3180e6f436e4ced51bf83cdb7c
Eric Hendershott <erich421@comcast.net>
2016-08-16 00:30:50 -0500

    Add images with carrierwave, S3 and Figaro

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'pg', group: :production
+gem 'carrierwave'
+gem 'fog'
+gem 'figaro'
 
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815041419) do
+ActiveRecord::Schema.define(version: 20160816040940) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160815041419) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
